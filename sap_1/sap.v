e829a9f424b88fdbfb33c6ece09682ea7aeaa0d4
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2023-03-26 18:04:17 +0100

    A button debouncer to prevent multiple pulses

--- sap_1/sap.v
+++ sap_1/sap.v
@@ -1,9 +1,27 @@
+`include "src/button_debounce.v"
+
 module sap(
     input clk,
+    input btn1,
     output reg [5:0] led
 );
 
+reg active;
+wire btn1_b;
+
+button_debounce bbtn(
+    .clk(clk),
+    .button_i(btn1),
+    .button_o(btn1_b)
+);
+
 always @(posedge clk) begin
-    led <= 6'b100110;
+    if (~btn1_b && ~active) begin
+        led <= ~(~led +1);
+        active <= 1;
+        end
+    else if(btn1_b)
+        active <= 0;
+    
 end
 endmodule
---
+++ sap_1/src/button_debounce.v
@@ -0,0 +1,26 @@
+/*
+    A button debounce module
+    clk: FPGA clock
+    button_i: The button to be debounced (active LOW)
+    button_o: The output to be used in place of button directly (active LOW
+*/
+module button_debounce(
+    input clk,
+    input button_i,
+    output button_o
+);
+
+reg [15:0] counter;
+
+always @(posedge clk) begin
+    if(~button_i) 
+        if (counter < 16'b111111111111111111)
+            counter <= counter + 1'd1;
+        else
+            button_o <= 0;
+    else
+        button_o <= 1;
+end
+
+
+endmodule
